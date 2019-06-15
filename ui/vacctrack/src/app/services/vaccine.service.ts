cbce767269569556ac3de727e415541f4bff773a
Ankit Patel <ankitpansuriya007@gmail.com>
2019-06-15 14:40:23 +0530

    vaccine list

--- ui/vacctrack/src/app/services/vaccine.service.ts
+++ ui/vacctrack/src/app/services/vaccine.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { ServerResponse } from 'src/app/models/response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VaccineService {
+  url = `${environment.API_ENDPOINT}vaccination/`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllVaccine(postData): Observable<any> {
+    return this.http.post(`${this.url}${postData.id}`, postData).pipe(
+      map((res: ServerResponse) => {
+        return res.data;
+      })
+    );
+  }
 }
