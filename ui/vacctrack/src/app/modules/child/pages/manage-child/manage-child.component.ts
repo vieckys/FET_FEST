6da8947dff633f6991573ada8a7636a396f6eec4
Ankit Patel <ankitpansuriya007@gmail.com>
2019-06-15 13:08:42 +0530

    child add

--- ui/vacctrack/src/app/modules/child/pages/manage-child/manage-child.component.ts
+++ ui/vacctrack/src/app/modules/child/pages/manage-child/manage-child.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { ChildService } from 'src/app/services/child.service';
 
 @Component({
   selector: 'app-manage-child',
@@ -9,33 +11,35 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class ManageChildComponent implements OnInit {
   childForm: FormGroup;
 
-  constructor() { }
+  constructor(private authService: AuthService, private childService: ChildService) { }
 
   ngOnInit() {
     this.initForm();
   }
 
   initForm() {
-  let name: string;
-  let dob: string;
-  let age: number;
-  let gender: string;
-  let weight: string;
-  let height: string;
-  let comments: string;
+    let name: string;
+    let dob: string;
+    let gender: string;
+    let weight: string;
+    let height: string;
+    let comments: string;
+    let user_id = this.authService.getUser().id;
 
     this.childForm = new FormGroup({
       name: new FormControl(name, Validators.required),
       dob: new FormControl(dob, Validators.required),
-      age: new FormControl(age, Validators.required),
       gender: new FormControl(gender, Validators.required),
       weight: new FormControl(weight, Validators.required),
       height: new FormControl(height, Validators.required),
-      comments: new FormControl(comments, Validators.required)
+      comments: new FormControl(comments, Validators.required),
+      user_id: new FormControl(user_id)
     });
   }
 
   onSubmit() {
+    this.childService.add(this.childForm.value);
+
     console.log(this.childForm.value);
   }
 
--- ui/vacctrack/src/app/services/child.service.ts
+++ ui/vacctrack/src/app/services/child.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChildService {
+  url = `${environment.API_ENDPOINT}children/`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  add(postData) {
+    this.http.post(`${this.url}`, postData).subscribe(res => {
+      console.log(res);
+    });
+  }
 }
