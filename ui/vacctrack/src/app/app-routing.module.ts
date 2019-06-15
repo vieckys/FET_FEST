fa066078bfc2c1419ac6d7eeebca4cc11651441f
Ankit Patel <ankitpansuriya007@gmail.com>
2019-06-15 12:48:45 +0530

    default route

--- ui/vacctrack/src/app/app-routing.module.ts
+++ ui/vacctrack/src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: './modules/home/home.module#HomeModule'
   },
