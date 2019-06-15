bf3f8a79c2496c2a91da1079bc11fa7df4d14b77
Ankit Patel <ankitpansuriya007@gmail.com>
2019-06-15 11:42:55 +0530

    desktop notification

--- ui/vacctrack/src/app/modules/dashboard/components/child-card/child-card.component.ts
+++ ui/vacctrack/src/app/modules/dashboard/components/child-card/child-card.component.ts
@@ -7,11 +7,55 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ChildCardComponent implements OnInit {
   @Input() child;
+
+  dayDiff: number = 2;
+
   constructor() {
    }
 
   ngOnInit() {
-    console.log('---->', this.child);
+    
+  }
+
+  notificationGranted = () => {
+    if (Notification && Notification.permission === "granted") {
+        return true;
+    } else if (Notification && Notification.permission !== "denied") {
+        
+        Notification.requestPermission(function (status) {
+            if (status === "granted") {
+              return true;
+            } else {
+              return false;
+            }
+        });
+
+    } else {
+        return false;
+    }
+  }
+
+  notifyMe(data) {
+    console.log('----->', data);
+
+    if (this.dayDiff < 3) {
+      setInterval(() => {
+        this.createNotification(data);
+      }, 10000); 
+    }
+  }
+
+  createNotification(data) {
+    if(this.notificationGranted()){
+        var notification = new Notification('VaccTrack Notification', {
+          icon: '',
+          body: `Next Vaccine Due for ${data.name} on ${data.vaccine.date} | ${data.vaccine.name}`,
+          lang: 'en',
+          dir: 'auto'
+        });
+    } else {
+        alert('Notification DENIED!');
+    }
   }
 
 }
