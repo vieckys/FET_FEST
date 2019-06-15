92738476f9b6d194146f52f97688137495575878
Hemant Sankhla <sankhlahemant251@gmail.com>
2019-06-15 13:56:17 +0530

    changes in child service

--- ui/vacctrack/src/app/services/child.service.ts
+++ ui/vacctrack/src/app/services/child.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,26 @@ import { HttpClient } from '@angular/common/http';
 export class ChildService {
   url = `${environment.API_ENDPOINT}children/`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   add(postData) {
     this.http.post(`${this.url}`, postData).subscribe(res => {
       console.log(res);
     });
+  }
+
+  getAll(postData) {
+    this.http.post(`${this.url}byUser/${postData.id}`, postData).subscribe(res => {
+      console.log(res);
+    });;
+  }
+
+  editChild(postData): Observable<any> {
+    return this.http.post(`${this.url}byUser`, postData).pipe(
+      map((res: Response) => {
+        return res;
+      }),
+      catchError(d => d)
+    );
   }
 }
