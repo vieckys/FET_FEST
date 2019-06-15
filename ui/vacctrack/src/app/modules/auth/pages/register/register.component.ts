2f9e83e0abca5e888789ebc74ab5c658bfac8733
Hemant Sankhla <sankhlahemant251@gmail.com>
2019-06-15 11:18:51 +0530

    registe form, did you know component

--- ui/vacctrack/src/app/modules/auth/pages/register/register.component.ts
+++ ui/vacctrack/src/app/modules/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    const email = '';
+    const name = '';
+    const password = '';
+
+    this.registerForm = new FormGroup({
+      email: new FormControl(email, Validators.required),
+      name: new FormControl(name, Validators.required),
+      password: new FormControl(password, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.registerForm.value);
   }
 
 }
